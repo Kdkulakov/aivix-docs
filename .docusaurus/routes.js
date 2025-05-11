d92ff57ee9ddcf6e4d599fb4732691eaf86e8ee8
kulakov <kdkulakov@gmail.com>
2025-05-11 09:46:29 +0300

    Next fix

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -39,13 +39,28 @@ export default [
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', 'bbe'),
+    component: ComponentCreator('/docs', 'dc1'),
     routes: [
+      {
+        path: '/docs/concepts',
+        component: ComponentCreator('/docs/concepts', 'bba'),
+        exact: true
+      },
+      {
+        path: '/docs/creating-first-agent',
+        component: ComponentCreator('/docs/creating-first-agent', 'db7'),
+        exact: true
+      },
       {
         path: '/docs/getting-started',
         component: ComponentCreator('/docs/getting-started', 'a24'),
         exact: true,
         sidebar: "tutorialSidebar"
+      },
+      {
+        path: '/docs/n8n-integration',
+        component: ComponentCreator('/docs/n8n-integration', '378'),
+        exact: true
       }
     ]
   },
